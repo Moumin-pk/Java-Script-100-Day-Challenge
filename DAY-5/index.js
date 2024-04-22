10c190b688b36828c501d1fe7777be7bf3c80f14
Moumin Ahmad <116239910+Moumin-pk@users.noreply.github.com>
2024-04-22 22:52:40 +0500

    Ascending order

---
+++ DAY-5/index.js
@@ -0,0 +1,17 @@
+// Write a function to sort an array of number in an ascending order.
+
+// Example here
+function sortAscending(e){
+return  e.sort((a,b) => a-b)
+}
+
+console.log(sortAscending([5,3,1,8])) // output : [1,3,5,8]
+// console.log(sortAscending([5,3,10,8])); // output[3,5,8,10]
+
+
+// => todo Requirements:
+//? The function should take an array of numbers as input.
+//? It should return a new array with the number sorted in ascending ordr.
+//? The orginal array should remain unchanged.
+//? you are not allowed to use the built-in sort() method.
+
